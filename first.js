44d38fccf172befd60b1d4a6c6fccd1db3efe762
umemasultan <umemasultan11@gmail.com>
2025-02-17 13:48:39 +0500

    1st lecture

---
+++ first.js
@@ -0,0 +1,109 @@
+
+// // first code of javascript//
+// console.log("Welcome to Javascript Series")
+// console.log("Hello World")
+
+
+// // datatypes in javascrpit//
+
+// primitive datatypes//
+
+// string
+// Number
+// Boolean
+// null
+// undefined
+// BigInt
+// Symbol
+
+// //string//
+// Name="Umema"
+// console.log(Name)
+// password="umemakhan@123"
+// console.log(password)
+// city="karachi"
+// console.log(city)
+
+// //integers
+// age=19
+// typeof age
+// console.log(age)
+// price=19.34
+// console.log(price)
+
+// //NUll
+// x=null
+// console.log(x)
+
+// //undefined
+// y=undefined
+// console.log(y)
+
+// // boolean//
+
+// follow=true
+// console.log(follow)
+// unfollow=false
+// console.log(unfollow)
+
+// //bigint
+//   a=BigInt("132")
+// console.log(a)
+
+// //symbol
+
+
+// b=Symbol("hello")
+// console.log(b)
+//let can not be decleard but can be updated
+
+// let Name="umema"
+// Name="sultan"
+// Name="lahore"
+// console.log(Name)
+
+// block symbol  {}
+// {
+// myname="umema"
+// console.log(myname)
+// }
+
+// {
+//     myname="umema sultan"
+//     console.log(myname)
+// }
+
+
+// // const can not be decleard or updated
+
+// const Name="umema"
+// console.log(Name)
+
+
+
+//non primitive datatypes//
+
+//object
+const info={
+  
+fullName:"Umema",
+age:19,
+isPass:true,
+percentage:202
+}
+info["age"]=info ["age"]+1
+console.log(info)
+console.log(info["fullName"])
+console.log(info["age"])
+console.log(info["isPass"])
+console.log(info["percentage"])
+
+//For practice //
+// insta profile
+const profile={
+  userName:"Umema",
+isFollow:true,
+follow:20,
+followwing:10
+}
+console.log(profile)
